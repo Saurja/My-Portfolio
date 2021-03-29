d0e4616b6b8f054ae8733e88acad6129b1a80ab1
Saurja <saurjaghoshofficial@gmail.com>
2021-03-30 00:35:43 +0530

    version 1 beta

---
+++ js/index.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+    el: '#intro',
+    data: {
+        name: 'Saurja Ghosh',
+        work: 'Software & Web Dev'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        todos: [{
+            text: 'C/C++'
+        }, {
+            text: 'Java'
+        }, {
+            text: 'OOPS'
+        }, {
+            text: 'HTML5 & CSS'
+        }, {
+            text: 'JS'
+        }, {
+            text: 'PHP'
+        }, {
+            text: 'BOOTSTRAP'
+        }, {
+            text: 'SQL'
+        }, {
+            text: 'DBMS'
+        }, {
+            text: 'GIT & GITHUB'
+        }, {
+            text: 'UI/UX Design'
+        }, {
+            text: 'MS OFFICE'
+        }]
+    }
+})
