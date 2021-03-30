40f9bf1ab69c7aee0c939f573a6b033bb2048bf0
Saurja <saurjaghoshofficial@gmail.com>
2021-03-30 18:52:48 +0530

    Itteration 1 Ready

--- js/index.js
+++ js/index.js
@@ -42,16 +42,24 @@ let app3 = new Vue({
     data: {
         projects: [{
             name: 'Digital Wallet Web Application',
-            text: 'Developed a prototype of a digital wallet is a software-based system that securely stores users payment information and passwords for numerous payment methods and websites. With the help of this application a person can send and receive credit points to any user in the application.'
+            text: 'Developed a prototype of a digital wallet is a software-based system that with the help of this application, a person can send and receive credit points to any user in the application.',
+            link: "https://www.github.com/Saurja/digital-wallet"
         }, {
             name: "CNN to classify 14 chest diseases from a chest X-Ray dataset",
             text: "The project is developed to classify a dataset (*Here NIH data set) into 14 different types of common chest diseases with the help of CNN, to reduce the cost and the time required to find the pulmonary diseases"
         }, {
             name: "Covid 19 Detection Web App",
-            text: "The project is developed to classify covid-19, made into a Web App that can be accessed by anywhere , to reduce the cost and the time required to detect Covid-19."
+            text: "The project is developed to classify covid-19, made into a Web App that can be accessed by anywhere , to reduce the cost and the time required to detect Covid-19.",
+            link: "https://github.com/Saurja/Covid-19_Disease_Diagnosis"
         }, {
             name: "Covid 19 Dashboard",
-            text: "The Coronavirus dashboard provides an overview of the 2019 Novel Coronavirus COVID-19. It shows countries affected by covid from the top to bottom."
+            text: "The Coronavirus dashboard provides an overview of the 2019 Novel Coronavirus COVID-19. It shows countries affected by covid from the top to bottom.",
+            link: "https://github.com/Saurja/covid-dashboard"
         }]
+    },
+    methods: {
+        redirect_to: function (link) {
+            location.replace(link)
+        }
     }
 })
