f62bdee4845f6635ac13051c950b5b82b790e968
Saurja <saurjaghoshofficial@gmail.com>
2021-03-30 15:58:04 +0530

    Added Page 3

--- js/index.js
+++ js/index.js
@@ -36,3 +36,19 @@ let app2 = new Vue({
         }]
     }
 })
+
+let app3 = new Vue({
+    el: '#app-3',
+    data: {
+        projects: [{
+            name: 'Digital Wallet Web Application',
+            text: 'Developed a prototype of a digital wallet is a software-based system that securely stores users payment information and passwords for numerous payment methods and websites. With the help of this application a person can send and receive credit points to any user in the application.'
+        }, {
+            name: "CNN to classify 14 chest diseases from a chest X-Ray dataset",
+            text: "The project is developed to classify a dataset (*Here NIH data set) into 14 different types of common chest diseases with the help of CNN, to reduce the cost and the time required to find the pulmonary diseases"
+        }, {
+            name: "Covid 19 Detection Web App",
+            text: "The project is developed to classify covid-19, made into a Web App that can be accessed by anywhere , to reduce the cost and the time required to detect Covid-19"
+        }]
+    }
+})
